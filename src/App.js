8a3be58a47ef11505d0bd45404216ed070cce746
nickNtoukas <ntoukas.nick@gmail.com>
2022-06-15 10:09:27 -0400

    First commit with pseudo-code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
+
+// MVP
+// Create app tthat allows user to a song to a playlist and store in db
+
+// STRETCH
+// Have separate inputs for artist name and song
+
+// APP Component
+// Track state of songs [songs, setSongs]
+// Create function handleRemoveSong
+// Use useEffect and store onValue event listener within
+// Iterate through data using for in loop
+// Update songs state to match db values
+// Pass props down to form comp
+
+// HEADER Component (stateless)
+
+// Potentially havee a SONG component
+
+// FORM Component
+// Track state of input change [userInput, setUserInput] then set within handleInputChange function
+// Create handleSubmit function to prevent default form behaviour, create db ref and push to userInput state of db

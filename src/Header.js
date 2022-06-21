a268c3c6ab4d28e9e97dd1ec03ee9c9580ad09e6
nickNtoukas <ntoukas.nick@gmail.com>
2022-06-21 10:54:53 -0400

    Chnaged secondary font

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <header className='header'>
       <div className='header-container'>
         <h1>
-          Welcome to <span>songs that slap</span>
+          Welcome to...<span>songs that slap</span>
         </h1>
         <h3>The place to manage your definitive playlist</h3>
       </div>

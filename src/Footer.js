c5837282f8b8f98f54abc41616057648be10cb0a
nickNtoukas <ntoukas.nick@gmail.com>
2022-06-21 17:08:48 -0400

    First commit on branch

--- src/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className='footer'>
-      <p class='copyright-footer'>developed & designed by nick ntoukas 2022</p>
-    </footer>
-  );
-};
-
-export default Footer;
